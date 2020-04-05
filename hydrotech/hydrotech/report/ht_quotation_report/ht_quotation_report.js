f93491855470319648ff33f9598d059cc4e4c7d5
Santoshbb <santosh.baburao@gmail.com>
2020-04-05 17:30:17 +0000

    Leave Policy,Employee Grade,SSA,Quotation,Employee

--- hydrotech/hydrotech/report/ht_quotation_report/ht_quotation_report.js
+++ hydrotech/hydrotech/report/ht_quotation_report/ht_quotation_report.js
@@ -5,5 +5,19 @@
 frappe.query_reports["HT Quotation Report"] = {
 	"filters": [
 
+    {
+    "fieldname":"from_date",
+    "label" :("From Date"),
+    "fieldtype":"Date",
+    "default":frappe.datetime.month_start(date)
+    },
+
+    {
+    "fieldname":"to_date",
+    "label":("To Date"),
+    "fieldtype":"Date",
+    "default":get_today()
+    }
+
 	]
 };
--- hydrotech/hydrotech/report/ht_quotation_report/ht_quotation_report.py
+++ hydrotech/hydrotech/report/ht_quotation_report/ht_quotation_report.py
@@ -4,6 +4,20 @@
 from __future__ import unicode_literals
 # import frappe
 
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import flt, getdate
+
 def execute(filters=None):
 	columns, data = [], []
-	return columns, data
+	columns=get_columns()
+	data=get_data(filters)
+        return columns, data
+
+def get_columns():
+	return [
+                _("Quotation") + ":Link/Quotation:100",
+                _("Customer ID") + ":Link/Customer:100",
+                _("Customer Name") + ":Link/Customer:130"
+        ]
